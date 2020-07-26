814d9fbd496b120b5befcfead91f19aabd02aaab
Spensar <Spencer.Lejeune97@gmail.com>
2020-07-26 17:06:27 -0500

    Added in modal popup for settings cleaned up useless code

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ load();
 setInterval(function(){
     date_time();
     save();
-    //document.getElementById("textBox").onchange = function() {save()};
 },
 1000);
 
@@ -15,35 +14,28 @@ function date_time(){
     var info = new Date();
     //Date
     var date = info.toLocaleDateString('en-US', {getUTCDate: 'numeric'});
-    //console.log(date);
     document.getElementById("date").innerHTML = date;
 
     //Day of week
     var dayofweek = info.getDay();
     var dayArray = {0:"Sunday", 1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday"}
-    //console.log(dayArray[dayofweek]);
     document.getElementById("dayofweek").innerHTML = dayArray[dayofweek];
 
     //Time
     var time = info.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-    //console.log(time);
     document.getElementById("time").innerHTML = time;
 
     //Morning/Afternoon
     if(time.slice(-2) == 'AM'){
-        //console.log('Morning');
         document.getElementById("ampm").innerHTML = "Morning!"
     }
     else{
-        //console.log('Afternoon');
         document.getElementById("ampm").innerHTML = "Afternoon!"
     }
 }
 
 function load(){
-    //console.log("Checking for any changes")
     var tester = localStorage.getItem("TB");
-    console.log(tester);
     document.getElementById("textBox").innerHTML = tester;
 }
 
@@ -54,6 +46,6 @@ function save(){
     var test = document.getElementById("textBox").value ;
     //Sets local storage to what is in the textbox
     localStorage.setItem("TB", test);
-
-    //console.log(test);
 }
+
+//Resizing Testing
